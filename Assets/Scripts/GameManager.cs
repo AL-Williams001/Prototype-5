a81170acf5a93e59b86cef5297078ca6982530e6
AL-Williams001 <arthurlynnwilliams01@gmail.com>
2025-04-16 18:09:10 +0800

    Created a new UpdateScore method

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
@@ -15,7 +16,8 @@ public class GameManager : MonoBehaviour
     {
         StartCoroutine(SpawnTarget());
         score = 0;
-        scoreText.text = "Score: " + score;
+        UpdateScore(0);
+        
     }
 
     // Update is called once per frame
@@ -31,7 +33,14 @@ public class GameManager : MonoBehaviour
         yield return new WaitForSeconds(spawnRate);
         int index = Random.Range(0, targets.Count);
         Instantiate(targets[index]);
+        UpdateScore(5);
         }
         
+    }
+
+    void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
     }
 }
